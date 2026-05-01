f2b83d8ff9deb2080e4b6d413be27197111b4528
Alexander-Reyes9 <alexander_reyes@ucsb.edu>
2026-04-30 21:27:05 -0700

    Add UCSBOrganization form component, tests, and stories

---
+++ frontend/src/main/components/UCSBOrganization/UCSBOrganizationForm.jsx
@@ -0,0 +1,118 @@
+import { Button, Form, Row, Col } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router";
+
+function UCSBOrganizationForm({
+  initialContents,
+  submitAction,
+  buttonLabel = "Create",
+}) {
+  // Stryker disable all
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({ defaultValues: initialContents || {} });
+  // Stryker restore all
+
+  const navigate = useNavigate();
+
+  const testIdPrefix = "UCSBOrganizationForm";
+
+  return (
+    <Form onSubmit={handleSubmit(submitAction)}>
+      <Row>
+        {initialContents && (
+          <Col>
+            <Form.Group className="mb-3">
+              <Form.Label htmlFor="orgCode">Org Code</Form.Label>
+              <Form.Control
+                data-testid={testIdPrefix + "-orgCode"}
+                id="orgCode"
+                type="text"
+                {...register("orgCode")}
+                value={initialContents.orgCode}
+                disabled
+              />
+            </Form.Group>
+          </Col>
+        )}
+      </Row>
+
+      <Row>
+        <Col>
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="orgTranslationShort">
+              Organization Translation Short
+            </Form.Label>
+            <Form.Control
+              data-testid={testIdPrefix + "-orgTranslationShort"}
+              id="orgTranslationShort"
+              type="text"
+              isInvalid={Boolean(errors.orgTranslationShort)}
+              {...register("orgTranslationShort", {
+                required: "Organization Translation Short is required.",
+              })}
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.orgTranslationShort?.message}
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col>
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="orgTranslation">
+              Organization Translation
+            </Form.Label>
+            <Form.Control
+              data-testid={testIdPrefix + "-orgTranslation"}
+              id="orgTranslation"
+              type="text"
+              isInvalid={Boolean(errors.orgTranslation)}
+              {...register("orgTranslation", {
+                required: "Organization Translation is required.",
+              })}
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.orgTranslation?.message}
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col>
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="inactive">Inactive</Form.Label>
+            <Form.Check
+              data-testid={testIdPrefix + "-inactive"}
+              id="inactive"
+              type="checkbox"
+              {...register("inactive")}
+            />
+          </Form.Group>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col>
+          <Button type="submit" data-testid={testIdPrefix + "-submit"}>
+            {buttonLabel}
+          </Button>
+          <Button
+            variant="Secondary"
+            onClick={() => navigate(-1)}
+            data-testid={testIdPrefix + "-cancel"}
+          >
+            Cancel
+          </Button>
+        </Col>
+      </Row>
+    </Form>
+  );
+}
+
+export default UCSBOrganizationForm;
---
+++ frontend/src/tests/components/UCSBOrganization/UCSBOrganizationForm.test.jsx
@@ -0,0 +1,139 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router";
+
+import UCSBOrganizationForm from "main/components/UCSBOrganization/UCSBOrganizationForm";
+import { ucsbOrganizationFixtures } from "fixtures/ucsbOrganizationFixtures";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const mockedNavigate = vi.fn();
+vi.mock("react-router", async () => {
+  const originalModule = await vi.importActual("react-router");
+  return {
+    ...originalModule,
+    useNavigate: () => mockedNavigate,
+  };
+});
+
+describe("UCSBOrganizationForm tests", () => {
+  const queryClient = new QueryClient();
+
+  const expectedHeaders = [
+    "Organization Translation Short",
+    "Organization Translation",
+    "Inactive",
+  ];
+  const testId = "UCSBOrganizationForm";
+
+  test("renders correctly with no initialContents", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <UCSBOrganizationForm />
+        </Router>
+      </QueryClientProvider>,
+    );
+
+    expect(await screen.findByText(/Create/)).toBeInTheDocument();
+
+    expectedHeaders.forEach((headerText) => {
+      const header = screen.getByText(headerText);
+      expect(header).toBeInTheDocument();
+    });
+  });
+
+  test("renders correctly when passing in initialContents", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <UCSBOrganizationForm
+            initialContents={ucsbOrganizationFixtures.oneOrganization}
+          />
+        </Router>
+      </QueryClientProvider>,
+    );
+
+    expect(await screen.findByText(/Create/)).toBeInTheDocument();
+
+    expectedHeaders.forEach((headerText) => {
+      const header = screen.getByText(headerText);
+      expect(header).toBeInTheDocument();
+    });
+
+    expect(
+      await screen.findByTestId(`${testId}-orgCode`),
+    ).toBeInTheDocument();
+    expect(screen.getByText(/Org Code/)).toBeInTheDocument();
+    expect(screen.getByTestId(`${testId}-orgCode`)).toHaveValue("ZPR");
+  });
+
+  test("that navigate(-1) is called when Cancel is clicked", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <UCSBOrganizationForm />
+        </Router>
+      </QueryClientProvider>,
+    );
+    expect(await screen.findByTestId(`${testId}-cancel`)).toBeInTheDocument();
+    const cancelButton = screen.getByTestId(`${testId}-cancel`);
+
+    fireEvent.click(cancelButton);
+
+    await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith(-1));
+  });
+
+  test("that the correct validations are performed", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <UCSBOrganizationForm />
+        </Router>
+      </QueryClientProvider>,
+    );
+
+    expect(await screen.findByText(/Create/)).toBeInTheDocument();
+    const submitButton = screen.getByText(/Create/);
+    fireEvent.click(submitButton);
+
+    await screen.findByText(/Organization Translation Short is required./);
+    expect(
+      screen.getByText(/Organization Translation is required./),
+    ).toBeInTheDocument();
+  });
+
+  test("No error messages on good input", async () => {
+    const mockSubmitAction = vi.fn();
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <UCSBOrganizationForm submitAction={mockSubmitAction} />
+        </Router>
+      </QueryClientProvider>,
+    );
+
+    const orgTranslationShortField = screen.getByTestId(
+      `${testId}-orgTranslationShort`,
+    );
+    const orgTranslationField = screen.getByTestId(`${testId}-orgTranslation`);
+    const submitButton = screen.getByTestId(`${testId}-submit`);
+
+    fireEvent.change(orgTranslationShortField, {
+      target: { value: "ZETA PHI RHO" },
+    });
+    fireEvent.change(orgTranslationField, {
+      target: { value: "ZETA PHI RHO FRATERNITY" },
+    });
+    fireEvent.click(submitButton);
+
+    await waitFor(() => expect(mockSubmitAction).toHaveBeenCalled());
+
+    expect(
+      screen.queryByText(/Organization Translation Short is required./),
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByText(/Organization Translation is required./),
+    ).not.toBeInTheDocument();
+  });
+});
