f3714104da87bce08d1cbc828ce5612720052494
Hanson Hu <hanyu_hu@ucsb.edu>
2026-05-04 00:12:21 -0700

    copy placeholder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,10 @@ import PlaceholderIndexPage from "main/pages/Placeholder/PlaceholderIndexPage";
 import PlaceholderCreatePage from "main/pages/Placeholder/PlaceholderCreatePage";
 import PlaceholderEditPage from "main/pages/Placeholder/PlaceholderEditPage";
 
+import ArticlesCreatePage from "main/pages/Articles/ArticlesCreatePage";
+import ArticlesEditPage from "main/pages/Articles/ArticlesEditPage";
+import ArticlesIndexPage from "main/pages/Articles/ArticlesIndexPage";
+
 import { hasRole, useCurrentUser } from "main/utils/useCurrentUser";
 
 import "bootstrap/dist/css/bootstrap.css";
@@ -68,6 +72,21 @@ function App() {
           />
         </>
       )}
+      {
+      hasRole(currentUser, "ROLE_USER") && (
+        <>
+          <Route exact path="/articles" element={<ArticlesIndexPage />} />
+        </>
+      )
+}
+{
+      hasRole(currentUser, "ROLE_ADMIN") && (
+        <>
+          <Route exact path="/articles/edit/:id" element={<ArticlesEditPage />} />
+          <Route exact path="/articles/create" element={<ArticlesCreatePage />} />
+        </>
+      )
+}
       {hasRole(currentUser, "ROLE_USER") && (
         <>
           <Route exact path="/placeholder" element={<PlaceholderIndexPage />} />
