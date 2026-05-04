8c5023d4bf97d46453d0c8ebd287bf4941db6a90
Hanson Hu <hanyu_hu@ucsb.edu>
2026-05-03 21:03:26 -0700

    add table files

---
+++ frontend/src/main/components/Articles/ArticleTable.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import OurTable, { ButtonColumn } from "main/components/OurTable";
+
+import { useBackendMutation } from "main/utils/useBackend";
+import { onDeleteSuccess } from "main/utils/UCSBDateUtils";
+import { useNavigate } from "react-router";
+import { hasRole } from "main/utils/useCurrentUser";
+
+function cellToAxiosParamsDelete(cell) {
+  return {
+    url: "/api/articles",
+    method: "DELETE",
+    params: {
+      id: cell.row.original.id,
+    },
+  };
+}
+
+export default function ArticleTable({ dates, currentUser }) {
+  const navigate = useNavigate();
+
+  const editCallback = (cell) => {
+    navigate(`/articles/edit/${cell.row.original.id}`);
+  };
+
+  // Stryker disable all : hard to test for query caching
+
+  const deleteMutation = useBackendMutation(
+    cellToAxiosParamsDelete,
+    { onSuccess: onDeleteSuccess },
+    ["/api/articles/all"],
+  );
+  // Stryker restore all
+
+  // Stryker disable next-line all : TODO try to make a good test for this
+  const deleteCallback = async (cell) => {
+    deleteMutation.mutate(cell);
+  };
+
+  const columns = [
+    {
+      header: "id",
+      accessorKey: "id", // accessor is the "key" in the data
+    },
+    {
+      header: "Title",
+      accessorKey: "title",
+    },
+    {
+      header: "Url",
+      accessorKey: "url",
+    },
+    {
+      header: "Explanation",
+      accessorKey: "explanation",
+    },
+    {
+      header: "Email",
+      accessorKey: "email",
+    },
+    {
+      header: "DateAdded",
+      accessorKey: "dateAdded",
+    },
+  ];
+
+  if (hasRole(currentUser, "ROLE_ADMIN")) {
+    columns.push(
+      ButtonColumn("Edit", "primary", editCallback, "ArticleTable"),
+    );
+    columns.push(
+      ButtonColumn("Delete", "danger", deleteCallback, "ArticleTable"),
+    );
+  }
+
+  return <OurTable data={dates} columns={columns} testid={"ArticleTable"} />;
+}
