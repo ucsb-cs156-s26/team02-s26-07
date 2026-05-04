f2a02c363d9e64046c3c995171fb49d01c3f4e37
Hanson Hu <hanyu_hu@ucsb.edu>
2026-05-04 14:04:03 -0700

    page create& test and moved some files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -72,21 +72,25 @@ function App() {
           />
         </>
       )}
-      {
-      hasRole(currentUser, "ROLE_USER") && (
+      {hasRole(currentUser, "ROLE_USER") && (
         <>
           <Route exact path="/articles" element={<ArticlesIndexPage />} />
         </>
-      )
-}
-{
-      hasRole(currentUser, "ROLE_ADMIN") && (
+      )}
+      {hasRole(currentUser, "ROLE_ADMIN") && (
         <>
-          <Route exact path="/articles/edit/:id" element={<ArticlesEditPage />} />
-          <Route exact path="/articles/create" element={<ArticlesCreatePage />} />
+          <Route
+            exact
+            path="/articles/edit/:id"
+            element={<ArticlesEditPage />}
+          />
+          <Route
+            exact
+            path="/articles/create"
+            element={<ArticlesCreatePage />}
+          />
         </>
-      )
-}
+      )}
       {hasRole(currentUser, "ROLE_USER") && (
         <>
           <Route exact path="/placeholder" element={<PlaceholderIndexPage />} />
--- frontend/src/tests/pages/Articles/ArticlesCreatePage.test.jsx
+++ frontend/src/tests/pages/Articles/ArticlesCreatePage.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, waitFor, fireEvent, screen } from "@testing-library/react";
 import ArticlesCreatePage from "main/pages/Articles/ArticlesCreatePage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { MemoryRouter } from "react-router";
@@ -7,12 +7,32 @@ import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
-import { expect } from "vitest";
 
-describe("ArticlesCreatePage tests", () => {
+const mockToast = vi.fn();
+vi.mock("react-toastify", async (importOriginal) => {
+  const originalModule = await importOriginal();
+  return {
+    ...originalModule,
+    toast: vi.fn((x) => mockToast(x)),
+  };
+});
+
+const mockNavigate = vi.fn();
+vi.mock("react-router", async (importOriginal) => {
+  const originalModule = await importOriginal();
+  return {
+    ...originalModule,
+    Navigate: vi.fn((x) => {
+      mockNavigate(x);
+      return null;
+    }),
+  };
+});
+
+describe("ArticleCreatePage tests", () => {
   const axiosMock = new AxiosMockAdapter(axios);
 
-  const setupUserOnly = () => {
+  beforeEach(() => {
     axiosMock.reset();
     axiosMock.resetHistory();
     axiosMock
@@ -21,15 +41,10 @@ describe("ArticlesCreatePage tests", () => {
     axiosMock
       .onGet("/api/systemInfo")
       .reply(200, systemInfoFixtures.showingNeither);
-  };
+  });
 
-  const queryClient = new QueryClient();
-  test("Renders expected content", async () => {
-    // arrange
-
-    setupUserOnly();
-
-    // act
+  test("renders without crashing", async () => {
+    const queryClient = new QueryClient();
     render(
       <QueryClientProvider client={queryClient}>
         <MemoryRouter>
@@ -38,11 +53,68 @@ describe("ArticlesCreatePage tests", () => {
       </QueryClientProvider>,
     );
 
-    // assert
+    await waitFor(() => {
+      expect(screen.getByTestId("ArticleForm-dateAdded")).toBeInTheDocument();
+    });
+  });
 
-    await screen.findByText("Create page not yet implemented");
-    expect(
-      screen.getByText("Create page not yet implemented"),
-    ).toBeInTheDocument();
+  test("when you fill in the form and hit submit, it makes a request to the backend", async () => {
+    const queryClient = new QueryClient();
+    const article = {
+      id: 17,
+      title: "atitle",
+      url: "http://article.com",
+      explanation: "a sample article",
+      email: "authoremail@email.com",
+      dateAdded: "2022-02-02T00:00",
+    };
+
+    axiosMock.onPost("/api/articles/post").reply(202, article);
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <ArticlesCreatePage />
+        </MemoryRouter>
+      </QueryClientProvider>,
+    );
+
+    await waitFor(() => {
+      expect(screen.getByTestId("ArticleForm-dateAdded")).toBeInTheDocument();
+    });
+
+    const dateAdded = screen.getByTestId("ArticleForm-dateAdded");
+    const title = screen.getByTestId("ArticleForm-title");
+    const url = screen.getByTestId("ArticleForm-url");
+    const explanation = screen.getByTestId("ArticleForm-explanation");
+    const email = screen.getByTestId("ArticleForm-email");
+    const submitButton = screen.getByTestId("ArticleForm-submit");
+
+    fireEvent.change(title, { target: { value: "atitle" } });
+    fireEvent.change(url, { target: { value: "http://article.com" } });
+    fireEvent.change(explanation, { target: { value: "a sample article" } });
+    fireEvent.change(email, { target: { value: "email@email.com" } });
+    fireEvent.change(dateAdded, {
+      target: { value: "2022-02-02T00:00" },
+    });
+
+    expect(submitButton).toBeInTheDocument();
+
+    fireEvent.click(submitButton);
+
+    await waitFor(() => expect(axiosMock.history.post.length).toBe(1));
+
+    expect(axiosMock.history.post[0].params).toEqual({
+      title: "atitle",
+      url: "http://article.com",
+      explanation: "a sample article",
+      email: "email@email.com",
+      dateAdded: "2022-02-02T00:00",
+    });
+
+    expect(mockToast).toBeCalledWith(
+      "New Article Created - id: 17 title: atitle",
+    );
+    expect(mockNavigate).toBeCalledWith({ to: "/articles" });
   });
 });
