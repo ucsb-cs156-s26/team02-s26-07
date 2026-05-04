d17c371a40bbf119e4243977ca92620b50af9cb8
Raymond Xu <raymondxu@ucsb.edu>
2026-05-03 18:08:55 -0700

    rx - run prettier on menuitemreview js fixtures

--- frontend/src/fixtures/menuItemReviewFixtures.js
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -1,39 +1,39 @@
 const menuItemReviewFixtures = {
   oneMenuItemReview: [
     {
-      "id": 1,
-      "itemId": 3,
-      "reviewerEmail": "no@ucsb.edu",
-      "stars": 1,
-      "dateReviewed": "2026-05-20T12:00:00",
-      "comments": "tide pod cake sucks"
+      id: 1,
+      itemId: 3,
+      reviewerEmail: "no@ucsb.edu",
+      stars: 1,
+      dateReviewed: "2026-05-20T12:00:00",
+      comments: "tide pod cake sucks",
     },
   ],
 
   threeMenuItemReviews: [
     {
-      "id": 2,
-      "itemId": 1,
-      "reviewerEmail": "g@ucsb.edu",
-      "stars": 2,
-      "dateReviewed": "2026-05-04T12:00:00",
-      "comments": "turkey sucks"
+      id: 2,
+      itemId: 1,
+      reviewerEmail: "g@ucsb.edu",
+      stars: 2,
+      dateReviewed: "2026-05-04T12:00:00",
+      comments: "turkey sucks",
     },
     {
-      "id": 3,
-      "itemId": 2,
-      "reviewerEmail": "p2@ucsb.edu",
-      "stars": 3,
-      "dateReviewed": "2026-05-03T12:00:00",
-      "comments": "carnitas good"
+      id: 3,
+      itemId: 2,
+      reviewerEmail: "p2@ucsb.edu",
+      stars: 3,
+      dateReviewed: "2026-05-03T12:00:00",
+      comments: "carnitas good",
     },
     {
-      "id": 4,
-      "itemId": 3,
-      "reviewerEmail": "le@ucsb.edu",
-      "stars": 5,
-      "dateReviewed": "2026-05-01T12:00:00",
-      "comments": "jellybean on rye best"
+      id: 4,
+      itemId: 3,
+      reviewerEmail: "le@ucsb.edu",
+      stars: 5,
+      dateReviewed: "2026-05-01T12:00:00",
+      comments: "jellybean on rye best",
     },
   ],
 };
